4b987670941374e2bc1aecad4a75f7e9b378636b
JaydevM <JaydevM@users.noreply.github.com>
2019-02-23 23:34:45 +0530

    Correct Answer

--- NumberGuessing.java
+++ NumberGuessing.java
@@ -4,39 +4,43 @@ import java.io.*;
 public class Solution {
 	public static void main(String[] args) {
 		boolean somethingWentWrong = false;
-		Scanner in = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
+		Scanner in = new Scanner(System.in);
 		int t = Integer.parseInt(in.nextLine()); // Scanner has functions to read ints, longs, strings, chars, etc.
 		for (int i = 1; i <= t; ++i) {
-			String[] AB=in.nextLine().split(" ");
+			String[] AB = in.nextLine().split(" ");
 			int A = Integer.parseInt(AB[0]);
 			int B = Integer.parseInt(AB[1]);
 			int N = Integer.parseInt(in.nextLine());
 
 			while (true) {
-				System.out.println((A + B) / 2);
+				System.out.println((int) Math.ceil((A + B) / 2.0));
 				String res = in.nextLine();
 				if (res.equals("TOO_SMALL")) {
-					A = (A + B) / 2;
+					A = (int) Math.ceil((A + B) / 2.0);
 					continue;
 				} else if (res.equals("TOO_BIG")) {
-					B = (A + B) / 2;
+					B = (int) Math.ceil((A + B) / 2.0);
 					continue;
-				} else if (res.equals("CORRECT"))
+				} else if (res.equals("CORRECT")) {
 					break;
-				else if (res.equals("WRONG_ANSWER")) {
+				} else if (res.equals("WRONG_ANSWER")) {
 					somethingWentWrong = true;
-					break;
-				}
-				else{
+					in.close();
+					return;
+				} else {
 					somethingWentWrong = true;
-					break;
+					in.close();
+					return;
 				}
 			}
-			if (somethingWentWrong)
-				break;
+			if (somethingWentWrong) {
+				in.close();
+				return;
+			}
 
 			// System.out.println("Case #" + i + ": " + (n + m) + " " + (n * m));
 		}
 		in.close();
+		return;
 	}
 }
